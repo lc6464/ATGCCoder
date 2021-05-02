a17043438c5410815034d1eb6146fd8bcd964119
lc6464 <64722907+lc6464@users.noreply.github.com>
2021-05-02 17:14:53 +0800

    突然发现 Decoder 有点问题

--- Decoder.cs
+++ Decoder.cs
@@ -5,32 +5,6 @@ using System.Text.RegularExpressions;
 
 namespace ATGCCoder {
 	public class Decoder {
-		private static readonly Regex spiltRegex = new(@"^(\d\d)$");
-		private static readonly Regex matchRegex = new(@"^([01]{8})+$");
-		public static string EncodeBitString(string bitString) {
-			if (matchRegex.IsMatch(bitString)) {
-				StringBuilder sb = new();
-				foreach (var item in spiltRegex.Replace(bitString, " $1")[1..].Split(' ')) {
-					switch (item) {
-						case "00":
-							sb.Append('A');
-							break;
-						case "01":
-							sb.Append('T');
-							break;
-						case "10":
-							sb.Append('G');
-							break;
-						case "11":
-							sb.Append('C');
-							break;
-						default:
-							return $"Error: Can not encode \"{item}\" to ATGC-List!";
-					}
-				}
-				return sb.ToString();
-			}
-			return $"Error: Param \"bitString\" soes not meet the requirements!";
-		}
+		
 	}
 }
